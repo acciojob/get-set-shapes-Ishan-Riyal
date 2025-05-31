f4beb5b580169d73498f3f2bdb250d5352673d7c
Ishan Riyal <ishanriyal07@gmail.com>
2025-05-31 16:52:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -36,6 +36,19 @@ class Square extends Animal {
 
 }
 
+// Creating a Rectangle instance
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width);
+console.log(rectangle.height);
+console.log(rectangle.getArea()); 
+
+// Creating a Square instance
+const square = new Square(7);
+console.log(square.width);
+console.log(square.height);
+console.log(square.getArea());
+console.log(square.getPerimeter());
+
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
